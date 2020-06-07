18d7dcd963cc4540c4412e14448a162a95dfb4fa
manik8 <maniksharma2014.ms@gmail.com>
2020-06-08 03:43:37 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ require('./game.js');
 
 function createComponent() {
     const elem = document.createElement('h1');
-    elem.innerHTML = "Conway's life";
+    elem.innerHTML = "Conway life";
     return elem;
 }
 
